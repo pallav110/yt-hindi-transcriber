bbda9c89ac14647bb390f10356f2a634e88479ed
PythOn <pallavs2020@gmail.com>
2025-07-07 00:50:19 +0530

    minor fix

--- download_model.sh
+++ download_model.sh
@@ -2,6 +2,6 @@
 
 mkdir -p transcriber/vosk-model
 cd transcriber/vosk-model
-curl -LO https://alphacephei.com/vosk/models/vosk-model-hi-0.22.zip
+curl -LO https://alphacephei.com/vosk/models/vosk-model-small-hi-0.22.zip
 unzip vosk-model-hi-0.22.zip
 rm vosk-model-hi-0.22.zip
